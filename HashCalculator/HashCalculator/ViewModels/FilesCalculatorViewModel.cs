cb407ceec9123ba2f4615d050417003fe40bf40d
mariia_suvalova <mariia_suvalova@epam.com>
2017-09-10 15:44:55 +0300

    Clear code

--- HashCalculator/HashCalculator/ViewModels/FilesCalculatorViewModel.cs
+++ HashCalculator/HashCalculator/ViewModels/FilesCalculatorViewModel.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 using System.Threading;
